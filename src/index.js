a5db237d764aa13efb470fe3f5d04d70c09dcb34
Oscar Correa <oscar.correa@easesolutions.com>
2019-03-18 15:20:54 -0500

    Initial project base

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss";
+import "assets/demo/demo.css";
+
+// new imports start
+import { Provider } from "react-redux";
+import configureStore from "store";
+// new imports stop
+
+import indexRoutes from "routes/index.jsx";
+
+const hist = createBrowserHistory();
+
+// BEFORE
+// ReactDOM.render(
+//   <Router history={hist}>
+//     <Switch>
+//       {indexRoutes.map((prop, key) => {
+//         return <Route path={prop.path} key={key} component={prop.component} />;
+//       })}
+//     </Switch>
+//   </Router>,
+//   document.getElementById("root")
+// );
+
+//AFTER
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <Router history={hist}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+            return <Route path={prop.path} key={key} component={prop.component} />;
+          })
+        }
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
