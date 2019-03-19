c1ad9bf28311b00a988150883d19750ba919c315
Oscar Correa <oscar.correa@easesolutions.com>
2019-03-19 14:14:11 -0500

    add component table to servers

--- src/variables/general.jsx
+++ src/variables/general.jsx
@@ -2,32 +2,62 @@
 // // // table head data and table body data for Tables view
 // #############################
 
-const thead = ["Name", "Country", "City", "Salary"];
+// const thead = ["Name", "Country", "City", "Salary"];
+// const tbody = [
+//   {
+//     className: "table-success",
+//     data: ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"]
+//   },
+//   {
+//     className: "",
+//     data: ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"]
+//   },
+//   {
+//     className: "table-info",
+//     data: ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"]
+//   },
+//   {
+//     className: "",
+//     data: ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"]
+//   },
+//   {
+//     className: "table-danger",
+//     data: ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"]
+//   },
+//   { className: "", data: ["Mason Porter", "Chile", "Gloucester", "$78,615"] },
+//   {
+//     className: "table-warning",
+//     data: ["Jon Porter", "Portugal", "Gloucester", "$98,615"]
+//   }
+// ];
+
+const thead = ["Name"];
 const tbody = [
   {
     className: "table-success",
-    data: ["Dakota Rice", "Niger", "Oud-Turnhout", "$36,738"]
+    data: ["ATV(test)"]
   },
   {
     className: "",
-    data: ["Minerva Hooper", "Curaçao", "Sinaai-Waas", "$23,789"]
+    data: ["CC(dev)"]
   },
   {
     className: "table-info",
-    data: ["Sage Rodriguez", "Netherlands", "Baileux", "$56,142"]
+    data: ["CC(prod)"]
   },
   {
     className: "",
-    data: ["Philip Chaney", "Korea, South", "Overland Park", "$38,735"]
+    data: ["NEURON(dev)"]
   },
   {
     className: "table-danger",
-    data: ["Doris Greene", "Malawi", "Feldkirchen in Kärnten", "$63,542"]
+    data: ["NEURON(test)"]
   },
-  { className: "", data: ["Mason Porter", "Chile", "Gloucester", "$78,615"] },
+  { className: "", 
+  data: ["PPM(test)"]},
   {
     className: "table-warning",
-    data: ["Jon Porter", "Portugal", "Gloucester", "$98,615"]
+    data: ["PPM Sandbox"]
   }
 ];
 
